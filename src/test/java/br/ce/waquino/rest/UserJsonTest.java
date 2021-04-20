6013ce0f135e46b4a59af7e39e82911ea060fb05
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-04-19 22:42:15 -0300

    feat: Incluindo validacoes avancadas

--- src/test/java/br/ce/waquino/rest/UserJsonTest.java
+++ src/test/java/br/ce/waquino/rest/UserJsonTest.java
@@ -85,7 +85,7 @@ public class UserJsonTest {
     public void listaRaiz() {
         given()
                 .when()
-                .get(url + "/4")
+                .get(url)
                 .then()
                 .statusCode(200)
                 .body("$", hasSize(3))
@@ -93,5 +93,31 @@ public class UserJsonTest {
                 .body("age[1]", is(25))
                 .body("filhos.name", hasItem(Arrays.asList("Zezinho")))
                 .body("salary", contains(2500));
+    }
+
+    @Test
+    public void verificacoesAvancadas() {
+        given()
+                .when()
+                .get(url)
+                .then()
+                .statusCode(200)
+                .body("$", hasSize(3))
+                .body("age.findAll{it == 25}.size()", is(1)) // busca usuarios com 25 anos de idade
+                .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+                .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+                .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina")) // busca um objeto especifico
+                .body("findAll{it.age <= 25}[-1].name", is("Maria Joaquina")) // busca ultimo item por conta do -1
+                .body("find{it.age <= 25}.name", is("Maria Joaquina")) // find busca 1 so registro, findall traz todos
+                .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia")) // busca tudo que em n no nome
+                .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina")) // busca todos os names que tem tamanho >10
+                .body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA")) // transforma em caixa alta
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA")) // busca objeto Maria e transforma em caixa alta
+                .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1))) // busca objeto com Maria, transforma em caixa alta e converte em Array
+                .body("age.collect{it * 2}", hasItems(60,50,40)) // multiplica o campo age dos objetos por 2
+                .body("id.max()", is(3)) // busca o maior id da colecao
+                .body("salary.min()", is(1234.5678f)) //busca o menor salario
+                .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001))) //soma os salarios
+                .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)));
     }
 }
